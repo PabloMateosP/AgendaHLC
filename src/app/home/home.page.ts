00e5b609d9ef241812b303c6efb1748830c1f775
unknown <pablomp1301@gmail.com>
2023-10-18 09:56:19 +0200

    Subida proyecto agenda

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,14 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  
+  users: any;
 
-  constructor() {}
+  constructor(private httpClient:HttpClient) {
+
+    // httpClient es un objeto de una librería que se encarga de hacer operaciones con http
+    this.users = this.httpClient.get('https://randomuser.me/api/?results=47').pipe(map((res : any) => res['results']));
+
+  }
 
 }
